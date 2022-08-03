6c1b5e01c80665fbffe67e6dc63c9575bcc646a5
Luca De Menego <ldemenego@THEMA.local>
2022-08-03 14:34:28 +0200

    Test buildInUltraWideCamera format

--- ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
+++ ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
@@ -4,6 +4,13 @@ import ARKit
 #if !DISABLE_TRUEDEPTH_API
 func createFaceTrackingConfiguration(_ arguments: Dictionary<String, Any>) -> ARFaceTrackingConfiguration? {
     if(ARFaceTrackingConfiguration.isSupported) {
+        let config = ARFaceTrackingConfiguration()
+        for videoFormat in ARFaceTrackingConfiguration.supportedVideoFormats {
+            if videoFormat.captureDeviceType == .builtInUltraWideCamera {
+                config.videoFormat = videoFormat
+                break
+            }
+        }
         return ARFaceTrackingConfiguration()
     }
     return nil
