c2deda05bc19322201b5b9a7f7c8163a90757c09
Luca <lucademenego99@gmail.com>
2023-10-05 15:37:10 +0200

    Remove light estimation from arfacetracking configuration

--- ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
+++ ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
@@ -5,6 +5,7 @@ import ARKit
 func createFaceTrackingConfiguration(_ arguments: Dictionary<String, Any>) -> ARFaceTrackingConfiguration? {
     if(ARFaceTrackingConfiguration.isSupported) {
         let config = ARFaceTrackingConfiguration()
+        config.isLightEstimationEnabled = false  // Set light estimation to false explicitly
         if #available(iOS 14.5, *) {
             for videoFormat in ARFaceTrackingConfiguration.supportedVideoFormats {
                 if videoFormat.captureDeviceType == .builtInUltraWideCamera {
