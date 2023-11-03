3865e0db26aeec52ccf5e871a8c967abf7dd3612
Luca <lucademenego99@gmail.com>
2023-11-03 09:32:49 +0100

    Restore ultra wide camera handling

--- ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
+++ ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
@@ -6,14 +6,14 @@ func createFaceTrackingConfiguration(_ arguments: Dictionary<String, Any>) -> AR
     if(ARFaceTrackingConfiguration.isSupported) {
         let config = ARFaceTrackingConfiguration()
         config.isLightEstimationEnabled = false  // Set light estimation to false explicitly
-        // if #available(iOS 14.5, *) {
-        //     for videoFormat in ARFaceTrackingConfiguration.supportedVideoFormats {
-        //         if videoFormat.captureDeviceType == .builtInUltraWideCamera {
-        //             config.videoFormat = videoFormat
-        //             break
-        //         }
-        //     }
-        // }
+        if #available(iOS 14.5, *) {
+            for videoFormat in ARFaceTrackingConfiguration.supportedVideoFormats {
+                if videoFormat.captureDeviceType == .builtInUltraWideCamera {
+                    config.videoFormat = videoFormat
+                    break
+                }
+            }
+        }
         return config
     }
     return nil
